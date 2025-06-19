8db545a52b0d991e3f1ed101458ef70d10e0a757
bahmoussa <bah1999moussa@gmail.com>
2025-06-19 15:54:06 +0200

    Ajout de la views customerWithOrderLines sur items

--- src/main/java/springboot/projetfinal/model/Customer.java
+++ src/main/java/springboot/projetfinal/model/Customer.java
@@ -24,11 +24,11 @@ public class Customer extends Authentification {
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     private Collection<Order> orders = new ArrayList<>();
 
-    @JsonView(JsonViews.CustomerWithAll.class)
+    @JsonView({JsonViews.CustomerWithAll.class, JsonViews.CustomerWithOrdersWithOrderLines.class})
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     private Collection<Address> addresses = new ArrayList<>();
 
-    @JsonView(JsonViews.CustomerWithAll.class)
+    @JsonView({JsonViews.CustomerWithAll.class, JsonViews.CustomerWithOrdersWithOrderLines.class})
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     private Collection<Reservation> reservations = new ArrayList<>();
 
